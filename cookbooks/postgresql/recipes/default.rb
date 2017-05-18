b4b87c715d5f5cc81f888b19e600eae4d52087eb
Sunil Shivamallu <sunilshivamallu@gmail.com>
2017-05-18 00:32:35 +0000

    postgresql code edited

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -5,12 +5,12 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 package 'postgresql-server' do
-	action :nothing
 	notifies :run,'execute[postgresql-init]'
 end
 
 execute 'postgresql-init' do
 	command 'postgresql-setup initdb'
+	action :nothing
 end
 
 service 'postgresql' do
